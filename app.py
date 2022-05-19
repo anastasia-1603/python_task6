9e9fd6b080b470483850507e5c58192734a7858f
Anastasia Lazutkina <lazutkina.anastasia@mail.ru>
2022-05-19 22:40:16 +0300

    можно менять месяц и год

--- app.py
+++ app.py
@@ -1,30 +1,36 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 import logic
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/',  methods=['GET', 'POST'])
 def start_page():
-    month = int(request.args["month"])
-    year = int(request.args["year"])
-    day = int(request.args["day"])
-    # month = logic.get_curr_month()
-    # year = logic.get_curr_year()
+    if 'month' in request.form:
+        month = int(request.form["month"])
+    else:
+        month = logic.get_curr_month()
+    if 'year' in request.form:
+        year = int(request.form["year"])
+    else:
+        year = logic.get_curr_year()
+    if 'day' in request.form:
+        day = int(request.form["day"])
+    else:
+        day = logic.get_curr_day()
     days_list = logic.get_days_list(year, month)
-    month_name = logic.month_num_to_name(month, "en")
-    return render_template('not_logged_in.html', day=day, month=month_name, year=year,
-                           days_list=days_list, curr_year=year)
+    return render_template('not_logged_in.html', day=day, month=month, year=year, days_list=days_list,
+                           curr_year=logic.get_curr_year())
 
 
-@app.route('/select_month', methods=["GET"])
+@app.route('/',  methods=['GET', 'POST'])
 def select_month():
-    month = int(request.form['months'])
-    year = 2022
-    month_name = logic.month_num_to_name(month, "en")
+    year = int(request.form["year"])
+    day = int(request.form["day"])
+    month = int(request.form.get('month'))
     days_list = logic.get_days_list(year, month)
-    return render_template('not_logged_in.html', day=10, month=month_name, year=year,
-                           days_list=days_list, curr_year=year)
+    return render_template('not_logged_in.html', day=day, month=month, year=year, days_list=days_list,
+                           curr_year=logic.get_curr_year())
 
 
 if __name__ == "__main__":
